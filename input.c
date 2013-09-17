d91e21d40a3198287521fda50c308274959474c5
Hugo Trippaers <trippie@gmail.com>
2013-09-17 19:57:50 +0800

    Mac OS X Fix : No dangling else errors

--- input.c
+++ input.c
@@ -357,7 +357,7 @@ static void tokenise_line( zword_t char_buf, zword_t token_buf, zword_t dictiona
       /* Skip to next token */
 
       cp = next_token( cp, str_end, &token, &token_length, punctuation );
-      if ( token_length )
+      if ( token_length ) {
 
          /* If still space in token buffer then store word */
 
@@ -391,6 +391,7 @@ static void tokenise_line( zword_t char_buf, zword_t token_buf, zword_t dictiona
             output_string( "Too many words typed, discarding: " );
             output_line( token );
          }
+      }
    }
    while ( token_length );
 
